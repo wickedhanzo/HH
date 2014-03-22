99d852332cd2577a48eb63022e50ebe26de74d1b
Wickedhanzo <hansvangestel7@gmail.com>
2014-03-22 14:56:36 +0100

    HouseHoldIndex: Added FirstName and lastname to screen

--- src/HouseHoldApp.MVC/Models/UserModel.cs
+++ src/HouseHoldApp.MVC/Models/UserModel.cs
@@ -3,5 +3,7 @@
     public class UserModel
     {
         public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
